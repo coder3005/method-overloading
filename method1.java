9b1a500f504923133715830c57b9aaf4cf72a90b
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-04 22:47:41 +0530

    Create method1.java

---
+++ method1.java
@@ -0,0 +1,27 @@
+/*method overloading*/
+class A 
+{
+    int add()
+    {
+        int a=10, b=20, c;
+        c=a+b;
+        return c;
+    }
+    void add(int x, int y)
+    {
+        int c;
+        c=x+y;
+        System.out.println(c);
+    }
+    void add(int x,double y)
+    {
+        double c
+        c=x+y;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        A r=new A();
+        int add=r.add(); r.add(100,200); r.add(50,45.32);
+        System.out.println(add);
+    }
+}
